79acbc2904b154fb0e4794fc7ec0a8ef554ee6dd
Rina45 <90148827+Rina45@users.noreply.github.com>
2022-03-15 19:45:05 +0200

    check the store and logger

--- src/components/SingleQuote.js
+++ src/components/SingleQuote.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const SingleQuote = () => {
+    const quote = useSelector(state => state.quote);
+    const dispatch = useDispatch();
+    dispatch({ type: 'MAKE_ACTION' });
     return (
         <>
             <q>quote from someone</q>
+            <p>{quote}</p>
         </>
     );
 };
--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -2,13 +2,9 @@ import { applyMiddleware, createStore } from "redux";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 import quoteReducer from "./quoteReducer";
-// import "dotenv/config"
 
-const middleware = [thunk];
-// if (process.env.ENVIRONMENT !== 'production') {
 const logger = createLogger({ collapsed: true });
-middleware.push(logger);
-// }
+const middleware = [thunk, logger];
 const store = createStore(quoteReducer, applyMiddleware(...middleware));
 
 export default store;
