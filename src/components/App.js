e2fa5149f1b9db5718015c8810603a18f10c6962
Rina45 <90148827+Rina45@users.noreply.github.com>
2022-03-20 21:49:25 +0200

    and random button compnent

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Footer from './Footer';
 import QuotesArray from './QuotesArray';
+import RandomButton from './RandomButton';
 import SingleQuotePage from './SingleQuotePage';
 
 const App = () => {
     return (<>
-
+        <RandomButton />
         <SingleQuotePage />
         <QuotesArray />
         <Footer />
---
+++ src/components/RandomButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RandomButton = () => {
+    return (
+        <button>random</button>
+    );
+}
+
+export default RandomButton;
