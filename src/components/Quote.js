0aa93bda1d5c9881ed67f08cd5376359a05a7aeb
Rina45 <90148827+Rina45@users.noreply.github.com>
2022-03-20 21:07:20 +0200

    add font style

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import '../styles/Quote.module.css';
+import style from '../styles/Quote.module.css';
 
 const Quote = () => {
     const quote = useSelector(state => state.quote);
     const dispatch = useDispatch();
     dispatch({ type: 'MAKE_ACTION' });
     return (
-        <>
-            <q>{quote} Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ea. Optio sint cumque enim non perferendis ad saepe facilis amet temporibus repellat esse ex eum eos at beatae reiciendis totam, magnam rerum! Reiciendis optio modi obcaecati illo ipsa! Vitae officiis voluptate, amet libero explicabo ipsum aliquam quo! Debitis tempore accusantium similique dicta inventore fugit sequi itaque, explicabo dolorum neque excepturi vitae saepe omnis, quod cum earum! Cupiditate alias eius eum veritatis officia facere repellat, blanditiis magni provident sunt ea doloribus voluptas adipisci deleniti, quam molestias! Exercitationem voluptatum obcaecati, tenetur modi veniam dolorem, dolores reprehenderit quis, maxime esse illo commodi rem.</q>
-        </>
+        <div className={style.quoteBlock}>
+            <div className={style.quoteLine}></div>
+            <q>{quote}</q>
+        </div>
     );
 };
 
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,5 @@
 const initialState = {
-    quote: "working"
+    quote: "The first rule of any technology used in a business is that automation applied to an efficient operation will magnify the efficiency.The second is that automation applied to an inefficient operation will magnify the inefficiency."
 };
 
 export default initialState;
