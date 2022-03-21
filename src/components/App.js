f93a367a0b6c0e1b7d0f3256ad2d11493d2d0377
Rina45 <90148827+Rina45@users.noreply.github.com>
2022-03-21 15:46:26 +0200

    change quote font size

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
     return (<>
         <RandomButton />
         <SingleQuotePage />
-        <QuotesArray />
+        {/* <QuotesArray /> */}
         <Footer />
     </>);
 };
