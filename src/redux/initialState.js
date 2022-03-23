9a9e96b2657cb08dce34cc80d4a08e091d07ad62
Rina45 <90148827+Rina45@users.noreply.github.com>
2022-03-23 21:05:17 +0200

    change initalState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,8 @@
 const initialState = {
-    quote: "The first rule of any technology used in a business is that automation applied to an efficient operation will magnify the efficiency.The second is that automation applied to an inefficient operation will magnify the inefficiency."
+    quoteText: "The first rule of any technology used in a business is that automation applied to an efficient operation will magnify the efficiency.The second is that automation applied to an inefficient operation will magnify the inefficiency.",
+    author: "",
+    genre: "",
+    quoteArray: []
 };
 
 export default initialState;
