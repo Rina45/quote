76c8740526030c1db8c333f4518c8e12342bb972
Rina45 <90148827+Rina45@users.noreply.github.com>
2022-03-24 00:38:48 +0200

    fix css bug skeleton

--- src/components/LoadingSkeleton.js
+++ src/components/LoadingSkeleton.js
@@ -2,12 +2,7 @@ import React from 'react';
 import style from '../styles/LoadingSkeleton.module.css';
 
 const LoadingSkeleton = () => {
-    return (<>
-        <div className={style.skeleton}></div>
-        <div className={style.skeleton}></div>
-        <div className={style.skeleton}></div>
-        <div className={style.skeleton}></div>
-    </>);
+    return (<div className={style.skeleton}></div>);
 }
 
 export default LoadingSkeleton;
