8df1867bf85b5664e0190830bfe4e7818af97fdb
Rina45 <90148827+Rina45@users.noreply.github.com>
2022-03-31 21:53:22 +0300

    add href to github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import style from '../styles/Footer.module.css';
 const Footer = () => {
     return (
         <footer>
-            <p>created by <a href='#' className={style.user}>rina45</a> <a href='#' className={style.devChallenges}>- devChallenges.io</a></p>
+            <p>created by <a href='https://github.com/rina45' className={style.user}>rina45</a> <a href='https://github.com/rina45' className={style.devChallenges}>- devChallenges.io</a></p>
         </footer>
     );
 }
